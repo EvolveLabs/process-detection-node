028a5becb7b7919dd2118a5d2e08d591cf084401
Bob Black <git@riblack.com>
2016-07-24 16:17:35 -0500

    Switch to .query as universal method

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
 var libPs = require('./lib/ps');
 var libWmic = require('./lib/wmic');
 
-// returns pid and commands, WITHOUT args
-module.exports.list = function (callback){
-
+exports.query = function() {
     if (process.platform === 'win32'){
-        // Windows check
-        libWmic.query('', callback);
+        libWmic.query.apply(libWmic, arguments);
     } else {
-        // OS X/Linux check
-        libPs.query('-A -o pid,comm', callback);
-    }
-};
-
-module.exports.lookup = function (pid, callback){
-
-    if (process.platform === 'win32'){
-        // Windows check
-        libWmic.query('where processid=' + pid, callback);
-    } else {
-        // OS X/Linux check
-        libPs.query('-o pid,comm -p ' + pid, callback);
-    }
-};
-
-module.exports.detailedLookup = function (pid, callback){
-
-    if (process.platform === 'win32'){
-        // Windows check
-        libWmic.query('where processid=' + pid, callback);
-    } else {
-        // OS X/Linux check
-        libPs.query('-o pid,args -p ' + pid, callback);
+        libPs.query.apply(libPs, arguments);
     }
 };
--- test/test.js
+++ test/test.js
@@ -4,12 +4,70 @@ var sinon = require('sinon');
 var fs = require('fs');
 var path = require('path');
 var lab = exports.lab = Lab.script();
-var expect = Code.expect;
 var describe = lab.describe;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
+var expect = Code.expect;
 var it = lab.it;
 
 var libExec = require('../lib/exec');
+var libPs = require('../lib/ps');
+var libWmic = require('../lib/wmic');
 var ps = require('../index.js');
+
+describe('query', function() {
+    var sandbox;
+
+    beforeEach(function (done) {
+        sandbox = sinon.sandbox.create();
+        done();
+    });
+
+    afterEach(function (done) {
+        sandbox.restore();
+        done();
+    });
+
+    describe('when platform is darwin', function() {
+        beforeEach(function (done) {
+            Object.defineProperty(process, 'platform', {
+                value: 'darwin'
+            });
+            done();
+        });
+
+        it('should envoke libPs.auery', function(done) {
+            var arg1 = {};
+            var arg2 = {};
+            sandbox.mock(libPs)
+                .expects('query').withExactArgs(arg1, arg2);
+
+            ps.query(arg1, arg2);
+            sandbox.verify();
+            done();
+        });
+    });
+
+    describe('when platform is win32', function() {
+        beforeEach(function (done) {
+            Object.defineProperty(process, 'platform', {
+                value: 'win32'
+            });
+            done();
+        });
+
+        it('should envoke libWmic.auery', function(done) {
+            var arg1 = {};
+            var arg2 = {};
+            sandbox.mock(libWmic)
+                .expects('query').withExactArgs(arg1, arg2);
+
+            ps.query(arg1, arg2);
+            sandbox.verify();
+            done();
+        });
+    });
+});
 
 describe.skip('on OS X', function(){
 
